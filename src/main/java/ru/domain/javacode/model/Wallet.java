6330b3c7d5bebc7e038bdb0778ca94f0b6f77405
VadiMayer <h4998998482@gmail.com>
2024-04-15 16:47:39 +0300

    editing Wallet

--- src/main/java/ru/domain/javacode/model/Wallet.java
+++ src/main/java/ru/domain/javacode/model/Wallet.java
@@ -1,19 +1,23 @@
 package ru.domain.javacode.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
-@Table(name = "wallet")
+@Table(name = "wallets")
 public class Wallet {
 
     @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
 
+    @NotNull
+    @Column(name = "balance")
     private BigDecimal balance;
 
     public Wallet(BigDecimal balance) {
