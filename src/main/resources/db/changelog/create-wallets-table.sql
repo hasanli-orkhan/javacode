2d7528dfe41d5566fc49970bc8c5e499a6d55584
VadiMayer <h4998998482@gmail.com>
2024-04-18 15:11:35 +0300

    last don't work version

--- src/main/resources/db/changelog/create-wallets-table.sql
+++ src/main/resources/db/changelog/create-wallets-table.sql
@@ -3,6 +3,7 @@
 -- changeset Vadim:create-wallets-table
 CREATE TABLE wallets
 (
-    id      UUID PRIMARY KEY NOT NULL,
+    id      SERIAL NOT NULL,
+    UUID    VARCHAR(128) NOT NULL,
     balance DECIMAL(19, 2)   NOT NULL
 );
